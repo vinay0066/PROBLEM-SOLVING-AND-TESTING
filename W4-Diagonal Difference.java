8dc5bcb34997481c678c3ce8a6004bb56c9c0bb1
vinay0066 <vtu29256@veltech.edu.in>
2026-09-12 09:51:18 +0530

    W4-Diagonal Difference.java

--- W4-Diagonal Difference.java
+++ W4-Diagonal Difference.java
@@ -12,13 +12,6 @@ import static java.util.stream.Collectors.toList;
 
 class Result {
 
-    /*
-     * Complete the 'diagonalDifference' function below.
-     *
-     * The function is expected to return an INTEGER.
-     * The function accepts 2D_INTEGER_ARRAY arr as parameter.
-     */
-
     public static int diagonalDifference(List<List<Integer>> arr) {
         int n = arr.size();
     int left = 0;
